d4c377f6b4adf917a0a9df660597ad012f49eb57
Glitch (indaginebreach) <none>
2021-12-28 15:23:09 +0000

    🌈🙉 Checkpoint ./server.js:40465745/2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app = express().use(express.static(__dirname + '/')),
 http = require('http').Server(app);
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "./index.html");
+  response.sendFile(__dirname + "index.html");
 });
 
 http.listen(port, function(){
