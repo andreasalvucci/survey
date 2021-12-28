fea582b09ac53573134674595d06d9eea4248742
Glitch (glitch-hello-node) <none>
2021-12-28 15:00:08 +0000

    💏🕶 Checkpoint ./package.json:40465745/10 ./server.js:40465745/930 ./index.html:40465745/25624 ./public/index.html:40465745/6406

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express');
+const app = new express();
 
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+app.get('/', function(request, response){
+    response.sendFile('public/index.html');
 });
