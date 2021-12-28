f34c7c559248a113b10b84382515d56f573876c9
Glitch (indaginebreach) <none>
2021-12-28 15:45:44 +0000

    🌏👁 Checkpoint ./server.js:40465745/855 ./package.json:40465745/15

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
-var port = 8080,
-express = require('express'),
-app = express().use(express.static(__dirname + '/')),
-http = require('http').Server(app);
+var port = 8080;
+var needle = require('needle');
+var express = require('express');
+var app = express().use(express.static(__dirname + '/'));
+var http = require('http').Server(app);
 
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "index.html");
+});
+
+app.get('/hibp', (req, res) => {
+  var sanitizer = require('string-sanitizer')
+  var email = req.query.email
+  console.log("SERVER: EMAIL: " + email)
+  if(!sanitizer.validate.isEmail(email)){
+    res.json({"error":"bad e-mail"})
+  }
+  var url='https://haveibeenpwned.com/api/v3/breachedaccount/'+email
+  var options={
+    accept: 'application/json',
+    content_type: 'application/json',
+    headers: {
+      'hibp-api-key': '06d8f69641eb43209225729887699b81',
+      'user-agent':'myApp'
+    }
+  }
+
+  needle('get',url,options)
+  .then(function(resp){
+    console.log(resp.body);
+    if(resp.body.length==0){
+      console.log('Non e stata trovata nessuna mail');
+      res.json({"ok":"ok"});
+    }
+    else {
+      res.send(resp.body);
+    }
+  })
+  .catch(e=>console.log(e))
+
 });
 
 http.listen(port, function(){
